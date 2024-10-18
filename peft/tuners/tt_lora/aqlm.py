d85c2abd40bab53963d78087496c058043ecdebe
Upadhayaya, Rajesh <upadraj@gatech.edu>
2024-10-17 20:19:26 -0600

    Remove unsued dispatcher.

--- peft/tuners/tt_lora/aqlm.py
+++ peft/tuners/tt_lora/aqlm.py
@@ -25,62 +25,6 @@ if is_aqlm_available():
     from aqlm import QuantizedLinear
 
 
-class AqlmLoraLinear(torch.nn.Module, TTLoraLayer):
-    def __init__(
-        self,
-        base_layer,
-        adapter_name: str,
-        r: int = 0,
-        lora_alpha: int = 1,
-        lora_dropout: float = 0.0,
-        init_lora_weights: bool = True,
-        use_rslora: bool = False,
-        **kwargs,
-    ):
-        super().__init__()
-        TTLoraLayer.__init__(self, base_layer)
-
-        self._active_adapter = adapter_name
-        self.update_layer(adapter_name, r, lora_alpha, lora_dropout, init_lora_weights, use_rslora)
-
-    def forward(self, x: torch.Tensor):
-        # note: logic differs from default Linear because merging is not supported
-        result = self.base_layer(x)
-
-        if self.disable_adapters:
-            return result
-
-        for active_adapter in self.active_adapters:
-            if active_adapter not in self.lora_A.keys():
-                continue
-            lora_A = self.lora_A[active_adapter]
-            lora_B = self.lora_B[active_adapter]
-            dropout = self.lora_dropout[active_adapter]
-            scaling = self.scaling[active_adapter]
-
-            requires_conversion = not torch.is_autocast_enabled()
-            if requires_conversion:
-                expected_dtype = result.dtype
-                x = x.to(lora_A.weight.dtype)
-
-            output = lora_B(lora_A(dropout(x)))
-            if requires_conversion:
-                output = output.to(expected_dtype)
-            output = output * scaling
-            result += output
-        return result
-
-    def __repr__(self) -> str:
-        rep = super().__repr__()
-        return "lora." + rep
-
-    # TODO: Check if it is better as suggested by users https://github.com/PanQiWei/AutoGPTQ/pull/102
-    # def reset_lora_parameters(self, adapter_name):
-    #     if adapter_name in self.lora_A.keys():
-    #         torch.nn.init.xavier_uniform_(self.lora_A[adapter_name].weight)
-    #         torch.nn.init.zeros_(self.lora_B[adapter_name].weight)
-
-
 def dispatch_aqlm(
     target: torch.nn.Module,
     adapter_name: str,
@@ -92,9 +36,5 @@ def dispatch_aqlm(
         target_base_layer = target.get_base_layer()
     else:
         target_base_layer = target
-
-    if is_aqlm_available() and isinstance(target_base_layer, QuantizedLinear):
-        new_module = AqlmLoraLinear(target, adapter_name, **kwargs)
-        target.qweight = target_base_layer.codes
 
     return new_module
